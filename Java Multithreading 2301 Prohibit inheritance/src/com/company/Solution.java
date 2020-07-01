6853b178a7e1c2934fc85f67f022da7dd386a6d8
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-07-02 01:12:04 +0300

    Add files via upload

---
+++ Java Multithreading 2301 Prohibit inheritance/src/com/company/Solution.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Solution {
+    public final class Listener {
+
+    public void onMouseDown(int x, int y) {
+
+    }
+    public void onMouseUp(int x, int y) {
+
+    }
+    }
+    public static void main(String[] args) {
+
+    }
+}
